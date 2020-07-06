25f75adb8fef1789889260be91a1076954855d89
Raj007-bit <66832143+Raj007-bit@users.noreply.github.com>
2020-07-06 15:25:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-(function (window) {
+(function () {
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
+for (var i = 0; i < names.length; i++) {
 
-for (var i in names) {
 
-  var firstLetter = ((names[i]).charAt(0)).toLowerCase();
+  var firstLetter = names[i].charAt(0).toLowerCase();
 
-  if (firstLetter == 'j') {
-     window.byeSpeaker.speak(names[i]);
+  if (firstLetter === 'j') {
+    byeSpeaker.speak(names[i]);
   } else {
-     window.helloSpeaker.speak(names[i]);
+    helloSpeaker.speak(names[i]);
   }
 }
-})(window);
+
+})();
